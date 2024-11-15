daeeeb4219d897651889150558d53448cf7d9407
caioocon <caiohenry18@gmail.com>
2024-11-15 20:16:09 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 using Prometheus;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+//criar swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pedido", Version = "v1" });
+});
+
 var app = builder.Build();
 app.UseRouting();
+// Configure o middleware do Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pedido");
+    c.RoutePrefix = string.Empty; // Define o Swagger UI na raiz (opcional)
+});
 
 app.UseHttpMetrics(); //para monitorar as requisições HTTP, exportando métricas para o Prometheus
-
 app.MapControllers();
 app.MapMetrics(); //para exportar as métricas do Prometheus
 
--- obj/Debug/net8.0/ProjetoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f448a25b757bf0bb6f00e25a9c8db2d0d97d2e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
